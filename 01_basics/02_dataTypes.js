c0f1c057a3afa5af59e9137e7e83c6128c80fe74
SMTP18 <smtp.programmer@gmail.com>
2025-05-20 17:52:40 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all your java script code as newer version (Js)
+
+// alert(3+3); // We are using nodejs , not browser
+
+let StringName="Test"
+let age=18
+
+
+/*
+dataTypes
+// Premetive DataTypes hai ye
+null   - standalone value hai	
+Undefined - when value not deifne in varibale	
+Boolean	- True/False
+Number	- 2 to power 53 range hai
+BigInt	- trading platform
+String	- "Test"
+Symbol	- Uniqueness kein liye
+
+Object -
+
+*/
+
+console.log(typeof age); // type Number
+console.log(typeof null); // type object hota hai humesa null ka
+console.log(typeof undefined); // type undefined hota hai
