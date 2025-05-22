bb9f0680604f7d895543f1fd4cc5867eb04b0f6b
SMTP18 <smtp.programmer@gmail.com>
2025-05-22 07:09:19 +0000

    Conversion opertaions

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,27 @@
+let score="33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33"=>number
+//"33abc"=> NAN ( Not A Number)
+// true=> 1 , false =>0
+
+let IsLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(IsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// IsLoggedIn=1 => True , IsLoggedIn=0 => False
+// IsLoggedIn="" => False
+// IsLoggedIn='Harry'; => True
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
