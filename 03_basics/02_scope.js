f5ea1f5b11375a00c6326a0cf6721cf1e6e78d23
SMTP <smtp.programmer@gmail.com>
2025-06-05 17:45:03 +0000

    scope part 1 and part 2 with mini hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -11,12 +11,59 @@ if(true){
 let a = 10;
 const b1 = 20;
 // var c = 30;
-console.log("INNER: " ,a);
+// console.log("INNER: " ,a);
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b1);
 // console.log(c);
 
- 
+//********************************VIDEO NUmber 22******************/
+
+function one(){
+    const username="harry_negi"
+
+    function two(){
+        const website="youtube"
+        // console.log(username);
+    }
+
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+if(true){
+    const username="harry";
+    if(username==="harry"){
+        const website=" youtube";
+        //console.log(username+website);
+    }
+   
+    // console.log(website);
+    
+
+}
+
+// console.log(username);
+
+
+//**************************     intersting points mini hoisting    **********************//
+
+addone(5);
+
+function addone(num){
+    return num+1
+}
+
+//  addone(5);
+
+addtwo(5);
+const addtwo=function(num){
+    return num+2
+}
+
+// addtwo(5);
