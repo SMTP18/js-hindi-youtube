d77ce5a22600041c7bd358f7514601db7f66a743
SMTP <smtp.programmer@gmail.com>
2025-06-05 17:20:32 +0000

    scopes 1

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,11 +1,22 @@
 //video number 21
 
+// let a = 10;
+// const b1 = 20;
+// var c = 300;
+
+let  a=300; //global scope 
+
+if(true){
+//local scope    
 let a = 10;
 const b1 = 20;
-var c = 30;
+// var c = 30;
+console.log("INNER: " ,a);
+
+}
 
 console.log(a);
-console.log(b1);
-console.log(c);
+// console.log(b1);
+// console.log(c);
 
  
