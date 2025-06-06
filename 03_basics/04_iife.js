8c11b63d4eba83c07206fbfcd67a36be8b1d0e56
SMTP <smtp.programmer@gmail.com>
2025-06-06 17:28:45 +0000

    iife js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+//video number 24
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
