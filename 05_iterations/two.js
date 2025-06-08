338a3e2ea2b3f0dd58907922f2e03d1eaebeaa0b
SMTP <smtp.programmer@gmail.com>
2025-06-08 09:25:40 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// Video number 28 while and do while loop
+
+let index=0
+while(index<=10){
+    //console.log(`Value of index is ${index}`);
+    index=index+2;
+    
+}
+
+let myArray=['flash','superman','batman'];
+let arr=0;
+
+while(arr<myArray.length){
+   // console.log(`value is ${myArray[arr]}`);
+    arr=arr+1;
+    
+}
+
+//*********do while loop ***************/
+
+// let score=11;
+let score=10;
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+    
+} while(score<=10);
