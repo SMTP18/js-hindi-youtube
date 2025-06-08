fed3f87d4904d1d5fed00c8e399b37f257e16fc6
SMTP <smtp.programmer@gmail.com>
2025-06-08 18:03:53 +0000

    reduce method

---
+++ 05_iterations/eight.js
@@ -0,0 +1,44 @@
+//reduce method ( Array reduce method)
+
+const myNums=[1,2,3];
+
+// const myTotal=myNums.reduce( function (accumulator ,currentValue) {
+
+//     console.log(`accumulator: ${accumulator} and currentValue ${currentValue}`);
+    
+
+// return accumulator+currentValue;
+// } , 0   )
+// 0 accumulator ki initialValue hai
+
+
+// abb arrow function mein try karte hai likhne ki
+
+const myTotal=myNums.reduce( (accumulator,currentValue) => accumulator+ currentValue ,0 )
+
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
